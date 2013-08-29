01e3b4723a6bbb5b7683e6581769b9dd882bc57b
Beatriz <beatrizrezener@gmail.com>
2013-08-29 20:54:57 -0300

    Teste NB

---
+++ Teste/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Teste Git</title>
+    </head>
+    <body>
+        <h1>Olá Mundo!<h1>
+        <?php
+               
+        ?>
+    </body>
+</html>

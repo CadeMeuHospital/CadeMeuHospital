44c283f1ed1eddc5727953f436c04b6b3f9baacf
Levi Moraes <levi.moraes@yahoo.com.br>
2013-09-24 22:19:58 -0300

    Pesquisa por uma UBS aleatoria

--- Banco de Dados/PesquisaBanco.php
+++ Banco de Dados/PesquisaBanco.php
@@ -14,22 +14,7 @@
  		$i = 0;
  		
  		$executar = mysql_query("SELECT * FROM ubs WHERE cod_unico LIKE 35365");	
- 		//$executar = mysql_query($pesquisa);
  		$total = mysql_result($executar, $i, "nom_estab");
  		echo $total;
-
- 	
-
- 		//while($i < $linha){
- 		
- 		
-
- 		//$nom_estab = mysql_result($result, $i,"nom_estab");
- 		//$cod_unico = mysql_result($result, $i,"cod_unico");
- 		//$dsc_bairro = mysql_result($result, $i,"dsc_bairro");
- 		//$dsc_cidade = mysql_result($result, $i,"dsc_cidade");
- 		//echo "$cod_unico  ---  $nom_estab --- $dsc_bairro --- $dsc_cidade <br>" ;
- 		//$i++;
- 		//}
  		
 ?>

59ccbb869209ef302a2bd7a6528b7ba35eeabaf5
paulohtfs <paulohtfs@gmail.com>
2013-10-04 12:42:00 -0300

    Aplicação do padrão singleton na classe "controllerProfileUBS"

--- Model/profileUBS.php
+++ Model/profileUBS.php
@@ -1,152 +1,151 @@
 <?php
 
-    class profileUBS{
+class profileUBS {
 
-        private $idUBS;
-        private $latitudeUBS;
-        private $longitudeUBS;
-        private $codMunic;
-        private $codCNES;
-        private $nameUBS;
-        private $descEnder;
-        private $descBairro;
-        private $dscCidade;        
-        private $phoneUBS;
-        private $physicStructureUBS;
-        private $adapOldPeople;
-        private $descriTools;
-        private $descMedicine;
+    private $idUBS;
+    private $latitudeUBS;
+    private $longitudeUBS;
+    private $codMunic;
+    private $codCNES;
+    private $nameUBS;
+    private $descEnder;
+    private $descBairro;
+    private $dscCidade;
+    private $phoneUBS;
+    private $physicStructureUBS;
+    private $adapOldPeople;
+    private $descriTools;
+    private $descMedicine;
 
-        function __construct($idUBS,$latitudeUBS, $longitudeUBS, $codMunic,$codCNES, $nameUBS, $descEnder,$descBairro, $descCidade, $phoneUBS,$physicStructureUBS,$adapOldPeople,$descriTools,$descMedicine){
-            $this->setIdUBS($idUBS);
-            $this->setLatitudeUBS($latitudeUBS);
-            $this->setLongitudeUBS($longitudeUBS);
-            $this->codMunic($codMunic);
-            $this->setCodCNES($codCNES);
-            $this->setNameUBS($nameUBS);           
-            $this->setDescEnder($descEnder);
-            $this->setDescBairro($descBairro);
-            $this->setDescCidade($descCidade);    
-            $this->setPhoneUBS($phoneUBS);
-            $this->setPhysicStructureUBS($physicStructureUBS);
-            $this->setAdapOldPeople($adapOldPeople);
-            $this->setDescriTools($descriTools);
-            $this->setDescMedicine($descMedicine);         
-        }
+    function __construct($idUBS, $latitudeUBS, $longitudeUBS, $codMunic, $codCNES, $nameUBS, $descEnder, $descBairro, $descCidade, $phoneUBS, $physicStructureUBS, $adapOldPeople, $descriTools, $descMedicine) {
+        $this->setIdUBS($idUBS);
+        $this->setLatitudeUBS($latitudeUBS);
+        $this->setLongitudeUBS($longitudeUBS);
+        $this->codMunic($codMunic);
+        $this->setCodCNES($codCNES);
+        $this->setNameUBS($nameUBS);
+        $this->setDescEnder($descEnder);
+        $this->setDescBairro($descBairro);
+        $this->setDescCidade($descCidade);
+        $this->setPhoneUBS($phoneUBS);
+        $this->setPhysicStructureUBS($physicStructureUBS);
+        $this->setAdapOldPeople($adapOldPeople);
+        $this->setDescriTools($descriTools);
+        $this->setDescMedicine($descMedicine);
+    }
 
-        public function getIdUBS() {
-            return $this->idUBS;
-        }
+    public function getIdUBS() {
+        return $this->idUBS;
+    }
 
-        public function setIdUBS($idUBS) {
-            $this->idUBS = $idUBS;
-        }
+    public function setIdUBS($idUBS) {
+        $this->idUBS = $idUBS;
+    }
 
-        public function getNameUBS() {
-            return $this->nameUBS;
-        }
+    public function getNameUBS() {
+        return $this->nameUBS;
+    }
 
-        public function setNameUBS($nameUBS) {
-            $this->nameUBS = $nameUBS;
-        }
+    public function setNameUBS($nameUBS) {
+        $this->nameUBS = $nameUBS;
+    }
 
-        public function getLatitudeUBS() {
-            return $this->latitudeUBS;
-        }
+    public function getLatitudeUBS() {
+        return $this->latitudeUBS;
+    }
 
-        public function setLatitudeUBS($latitudeUBS) {
-            $this->latitudeUBS = $latitudeUBS;
-        }
+    public function setLatitudeUBS($latitudeUBS) {
+        $this->latitudeUBS = $latitudeUBS;
+    }
 
-        public function getLongitudeUBS() {
-            return $this->longitudeUBS;
-        }
+    public function getLongitudeUBS() {
+        return $this->longitudeUBS;
+    }
 
-        public function setLongitudeUBS($longitudeUBS) {
-            $this->longitudeUBS = $longitudeUBS;
-        }
+    public function setLongitudeUBS($longitudeUBS) {
+        $this->longitudeUBS = $longitudeUBS;
+    }
 
-        public function getCodCNES() {
-            return $this->codCNES;
-        }
+    public function getCodCNES() {
+        return $this->codCNES;
+    }
 
-        public function setCodCNES($codCNES) {
-            $this->codCNES = $codCNES;
-        }
+    public function setCodCNES($codCNES) {
+        $this->codCNES = $codCNES;
+    }
 
-        public function getPhoneUBS() {
-            return $this->phoneUBS;
-        }
+    public function getPhoneUBS() {
+        return $this->phoneUBS;
+    }
 
-        public function setPhoneUBS($phoneUBS) {
-            $this->phoneUBS = $phoneUBS;
-        }
+    public function setPhoneUBS($phoneUBS) {
+        $this->phoneUBS = $phoneUBS;
+    }
 
-        public function getPhysicStructureUBS() {
-            return $this->physicStructureUBS;
-        }
+    public function getPhysicStructureUBS() {
+        return $this->physicStructureUBS;
+    }
 
-        public function setPhysicStructureUBS($physicStructureUBS) {
-            $this->physicStructureUBS = $physicStructureUBS;
-        }
+    public function setPhysicStructureUBS($physicStructureUBS) {
+        $this->physicStructureUBS = $physicStructureUBS;
+    }
 
-        public function getAdapOldPeople() {
-            return $this->adapOldPeople;
-        }
+    public function getAdapOldPeople() {
+        return $this->adapOldPeople;
+    }
 
-        public function setAdapOldPeople($adapOldPeople) {
-            $this->adapOldPeople = $adapOldPeople;
-        }
+    public function setAdapOldPeople($adapOldPeople) {
+        $this->adapOldPeople = $adapOldPeople;
+    }
 
-        public function getDescriTools() {
-            return $this->descriTools;
-        }
+    public function getDescriTools() {
+        return $this->descriTools;
+    }
 
-        public function setDescriTools($descriTools) {
-            $this->descriTools = $descriTools;
-        }
+    public function setDescriTools($descriTools) {
+        $this->descriTools = $descriTools;
+    }
 
-        public function getDescMedicine() {
-            return $this->descMedicine;
-        }
+    public function getDescMedicine() {
+        return $this->descMedicine;
+    }
 
-        public function setDescMedicine($descMedicine) {
-            $this->descMedicine = $descMedicine;
-        }
-        public function getCodMunic() {
-            return $this->codMunic;
-        }
+    public function setDescMedicine($descMedicine) {
+        $this->descMedicine = $descMedicine;
+    }
 
-        public function setCodMunic($codMunic) {
-            $this->codMunic = $codMunic;
-        }
+    public function getCodMunic() {
+        return $this->codMunic;
+    }
 
-        public function getDescEnder() {
-            return $this->descEnder;
-        }
+    public function setCodMunic($codMunic) {
+        $this->codMunic = $codMunic;
+    }
 
-        public function setDescEnder($descEnder) {
-            $this->descEnder = $descEnder;
-        }
+    public function getDescEnder() {
+        return $this->descEnder;
+    }
 
-        public function getDescBairro() {
-            return $this->descBairro;
-        }
+    public function setDescEnder($descEnder) {
+        $this->descEnder = $descEnder;
+    }
 
-        public function setDescBairro($descBairro) {
-            $this->descBairro = $descBairro;
-        }
+    public function getDescBairro() {
+        return $this->descBairro;
+    }
 
-        public function getDscCidade() {
-            return $this->dscCidade;
-        }
+    public function setDescBairro($descBairro) {
+        $this->descBairro = $descBairro;
+    }
 
-        public function setDscCidade($dscCidade) {
-            $this->dscCidade = $dscCidade;
-        }
+    public function getDscCidade() {
+        return $this->dscCidade;
+    }
 
+    public function setDscCidade($dscCidade) {
+        $this->dscCidade = $dscCidade;
+    }
 
-
-        }
+}
 
 ?>

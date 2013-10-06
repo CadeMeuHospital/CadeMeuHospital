4730fcac5b944beaadc86d425e1715089115cceb
levimoraes <levi.moraes@yahoo.com.br>
2013-10-06 13:33:36 -0300

    Tratamento de excessao na conexao com o BD

--- Utils/dataBaseConnection.php
+++ Utils/dataBaseConnection.php
@@ -4,11 +4,14 @@
         $username = "root";
         $password = "";
         $dbconnection = mysql_connect($server, $username, $password);
-        
-        if(!$dbconnection){
-            die ("Não foi possível conectar: " . mysql_error());
-        }
-        
+         
         $dataBase = mysql_select_db("cademeuhospital");
 
+        try {
+            (!$dbconnection) == TRUE;
+        } 
+        catch (Exception $e) {
+            echo ($e->getMessage());
+        }
+        
 ?>

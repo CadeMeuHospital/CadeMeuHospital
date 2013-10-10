117e9b0b7b15f2fcb7312bafefdf7f6ed333cdc9
paulohtfs <paulohtfs@gmail.com>
2013-10-09 21:12:42 -0300

    Refatoração arquivo "DataValidation.php"

--- Utils/DataValidation.php
+++ Utils/DataValidation.php
@@ -1,89 +1,98 @@
 <?php
 
-
 class DataValidation {
-    
-    public function validateNullFields($parameter){
-            return !(empty($parameter));
-            //retorna verdadeiro caso a variavel esteja vazia
-            //com isso, o valor false eh invertido e enviado como true
-        }
-    
-    public function validateName($nameUBS){
 
-            $validCharacters = '. abcdefghijklmnopqrstuvwxyzçãõáíóúàòìù';
-            
-            for ($i = 0; $i < strlen($nameUBS); $i++) {
-                $char = stripos($validCharacters, $nameUBS[$i]);
-                if(!$char){
-                    return 1;
-                }
-                
-                if($nameUBS[$i] == " " && $nameUBS[$i+1] == " "){
-                    return 2;   
-                }
+    public function validateNullFields($parameter) {
+        return !(empty($parameter));
+        //retorna verdadeiro caso a variavel esteja vazia
+        //com isso, o valor false eh invertido e enviado como true
+    }
+
+    public function validateName($nameUBS) {
+
+        $result = 0;
+        $validCharacters = '. abcdefghijklmnopqrstuvwxyzçãõáíóúàòìù';
+
+        for ($i = 0; $i < strlen($nameUBS); $i++) {
+            $char = stripos($validCharacters, $nameUBS[$i]);
+            if (!$char) {
+                $result = 1;
+            }
+
+            if ($nameUBS[$i] == " " && $nameUBS[$i + 1] == " ") {
+                $result = 2;
             }
         }
-        
-        public function validateDscCidade($dscCidade){
 
-            $validCharacters = '. abcdefghijklmnopqrstuvwxyzçãõáíóúàòìù';
-            
-            for ($i = 0; $i < strlen($dscCidade); $i++) {
-                $char = stripos($validCharacters, $dscCidade[$i]);
-                if(!$char){
-                    return 1;
-                }
-                
-                if($dscCidade[$i] == " " && $dscCidade[$i+1] == " "){
-                    return 2;   
-                }
+        return $result;
+    }
+
+    public function validateDscCidade($dscCidade) {
+
+        $result = 0;
+        $validCharacters = '. abcdefghijklmnopqrstuvwxyzçãõáíóúàòìù';
+
+        for ($i = 0; $i < strlen($dscCidade); $i++) {
+            $char = stripos($validCharacters, $dscCidade[$i]);
+            if (!$char) {
+                $result = 1;
+            }
+
+            if ($dscCidade[$i] == " " && $dscCidade[$i + 1] == " ") {
+                $result = 2;
             }
         }
-        
-        public function validateDescBairro($descBairro){
 
-            $validCharacters = '. abcdefghijklmnopqrstuvwxyzçãõáíóúàòìù';
-       
-            for ($i = 0; $i < strlen($descBairro); $i++) {
-                $char = stripos($validCharacters, $descBairro[$i]);
-                if(!$char){
-                    return 1;
-                }
-                
-                if($descBairro[$i] == " " && $descBairro[$i+1] == " "){
-                    return 2;   
-                }
+        return $result;
+    }
+
+    public function validateDescBairro($descBairro) {
+
+        $result = 0;
+        $validCharacters = '. abcdefghijklmnopqrstuvwxyzçãõáíóúàòìù';
+
+        for ($i = 0; $i < strlen($descBairro); $i++) {
+            $char = stripos($validCharacters, $descBairro[$i]);
+            if (!$char) {
+                $result = 1;
+            }
+
+            if ($descBairro[$i] == " " && $descBairro[$i + 1] == " ") {
+                $result = 2;
             }
         }
-        
-        public function validateCodMunic($codMunic){
-            
+
+        return $result;
+    }
+
+    public function validateCodMunic($codMunic) {
+
+        $result = FALSE;
+        $isNumeric = is_numeric($codMunic);
+
+        if ($isNumeric) {
+            $result = TRUE;
+        } else {
             $result = FALSE;
-            $isNumeric = is_numeric($codMunic);
-            
-            if($isNumeric){
-                $result = TRUE;
-            }else{
-                $result = FALSE;
-            }
-            
-            return $result;
         }
-        
-        public function validateCodCNES($codCNES){
-            
+
+        return $result;
+    }
+
+    public function validateCodCNES($codCNES) {
+
+        $result = FALSE;
+        $isNumeric = is_numeric($codCNES);
+
+        if ($isNumeric) {
+            $result = TRUE;
+        } else {
             $result = FALSE;
-            $isNumeric = is_numeric($codCNES);
-            
-            if($isNumeric){
-                $result = TRUE;
-            } else {
-                $result = FALSE;
-            }
-            
-            return $result;
         }
+
+        return $result;
+    }
+
 }
 
 ?>

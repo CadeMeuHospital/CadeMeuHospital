72cbbf0ca87d8a08641abdad35c0def59b99474a
paulohtfs <paulohtfs@gmail.com>
2013-10-10 01:13:45 -0300

    Evolução do teste ControllerProfileUBS

--- Teste/ControllerTest/ControllerProfileUBSTest.php
+++ Teste/ControllerTest/ControllerProfileUBSTest.php
@@ -1,4 +1,26 @@
 <?php
 
+require_once "/../../Controller/ControllerProfileUBS.php";
+
+class ControllerProfileUBSTest extends PHPUnit_Framework_TestCase {
+
+    protected $controllerProfileUBS;
+
+    protected function setUp() {
+        $this->controllerProfileUBS = new ControllerProfileUBS;
+    }
+
+    protected function tearDown() {
+        unset($this->controllerProfileUBS);
+    }
+
+    protected function getInstanceTest() {
+        
+        $result = $this->controllerProfileUBS->getInstanceControllerProfileUBS();
+        $this->assertNULL($result);
+        
+    }
+
+}
 
 ?>

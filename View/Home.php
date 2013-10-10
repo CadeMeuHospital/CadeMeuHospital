e03987750cc081d02aef8ca96190fcf77ecce5e9
Levi Moraes <levi.moraes@yahoo.com.br>
2013-10-10 19:18:54 -0300

    Caso de Uso - Localiza Usuario - 75%

--- View/Home.php
+++ View/Home.php
@@ -10,35 +10,46 @@
 		<script type="text/javascript" src="../View/shared/js/jquery.maskedinput.js"></script>
 		<script type="text/javascript" src="../View/shared/js/scripts.js"></script>
 		<script type="text/javascript" src="../V.iew/shared/js/jquery.price_format.1.8.min.js"></script>
-                <script src="http://localhost/CadeMeuHospital/Utils/locateUser.js"></script>
 		<link href="../shared/css/jquery-ui-1.10.3.custom.css" rel="stylesheet">
 		<title> Cadê Meu Hospital - Home</title>
                 
-                <script>  
-           function getLocation() {
-           navigator.geolocation.getCurrentPosition(showPosition,showError);             
-           var latlon=position.coords.latitude+","+position.coords.longitude;          
-          alert(latlon);
-                }
-                 </script>
                 
 	</head>
 	
-<body onload = "getLocation();">
-	<div class="root">
-		
+        <body>
+       <div class="root">                   
+           <button onclick="getLocation()"><img src="satellite.png" width="50px" heigth="50px"></button> 
 		<?php require '../view/shared/header.php';?>
-		
+            
 		<?php require '../view/shared/navigation_bar.php';?>
 		
 		<div class="content">
-			
-			
-			
-		</div>
+		<script type="text/javascript" src="../Utils/locateUser.js" ></script>	
+               <p id="demo">Click the button to get your coordinates:</p>
+           <script>
+           var x=document.getElementById("demo");
+            function getLocation()
+              {
+              if (navigator.geolocation)
+                {
+                navigator.geolocation.getCurrentPosition(showPosition);
+                 }
+              else{x.innerHTML="Geolocation is not supported by this browser.";}
+              }
+            function showPosition(position)
+              {
+              x.innerHTML="Latitude: " + position.coords.latitude + 
+              "<br>Longitude: " + position.coords.longitude;
+              }
+           </script> 
+           
+</body>
+                </div>
             <br /><br /><br /><br /><br /><br /><br /><br /><br />
 		<?php require '../view/shared/footer.php';?>
             </div>
+    
+   
 	</div>
 </body>
 </html>

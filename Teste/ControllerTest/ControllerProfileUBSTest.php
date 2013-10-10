2eca2719fb1c4ab6feaa53732568a7afa6c16263
paulohtfs <paulohtfs@gmail.com>
2013-10-10 01:40:38 -0300

    Correção do teste ControllerProfileUBS

--- Teste/ControllerTest/ControllerProfileUBSTest.php
+++ Teste/ControllerTest/ControllerProfileUBSTest.php
@@ -1,21 +1,21 @@
 <?php
 
-require_once "/../../Controller/ControllerProfileUBS.php";
+require_once dirname(__FILE__) . '/../../Controller/ControllerProfileUBS.php';
 
 class ControllerProfileUBSTest extends PHPUnit_Framework_TestCase {
 
     protected $controllerProfileUBS;
 
     protected function setUp() {
-        $this->controllerProfileUBS = new ControllerProfileUBS;
+        $this->controllerProfileUBS = ControllerProfileUBS::getInstanceControllerProfileUBS();
     }
 
     protected function tearDown() {
         unset($this->controllerProfileUBS);
     }
 
-    protected function getInstanceTest() {
-        
+    public function test() {
+
         $result = $this->controllerProfileUBS->getInstanceControllerProfileUBS();
         $this->assertNULL($result);
         

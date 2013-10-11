cdfbe23aa6c8cf43ae44711ef13f0a73d32b917e
paulohtfs <paulohtfs@gmail.com>
2013-10-11 20:28:38 -0300

    Eliminação de duplicidade de código em "DataValidation.php"

--- Utils/DataValidation.php
+++ Utils/DataValidation.php
@@ -5,88 +5,47 @@ include_once '/../Util/Exception/InvalidNameException.php';
 define('SIZECODMUNIC', 6);
 
 class DataValidation {
-    
+
     public static function throwTextFieldException($textField) {
-        
+
         if (!validateNullFields($textField)) {
             throw new InvalidNameException("Campo não pode ser nulo!");
         } else {
-            if (validateName($textField) == 1) {
+            if (validateTextField($textField) == 1) {
                 throw new InvalidNameException("Campo contém caracteres invalidos!");
             } else {
-                if (validateName($textField) == 2) {
+                if (validateTextField($textField) == 2) {
                     throw new InvalidNameException("Campo contém espaços seguidos!");
                 } else {
                     return TRUE;
                 }
             }
         }
-        
     }
-    
+
     public static function validateNullFields($parameter) {
         return !(empty($parameter));
-        //retorna verdadeiro caso a variavel esteja vazia
-        //com isso, o valor false eh invertido e enviado como true
     }
 
-    public static function validateName($nameUBS) {
+    public static function validateTextField($textField) {
 
         $result = 0;
         $validCharacters = '. abcdefghijklmnopqrstuvwxyzçãõáíóúàòìù';
 
-        for ($i = 0; $i < strlen($nameUBS); $i++) {
-            $char = stripos($validCharacters, $nameUBS[$i]);
+        for ($i = 0; $i < strlen($textField); $i++) {
+            $char = stripos($validCharacters, $textField[$i]);
             if (!$char) {
                 $result = 1;
             }
 
-            if ($nameUBS[$i] == " " && $nameUBS[$i + 1] == " ") {
+            if ($textField[$i] == " " && $textField[$i + 1] == " ") {
                 $result = 2;
             }
         }
 
         return $result;
     }
-
-    public static function validateDscCidade($dscCidade) {
-
-        $result = 0;
-        $validCharacters = '. abcdefghijklmnopqrstuvwxyzçãõáíóúàòìù';
-
-        for ($i = 0; $i < strlen($dscCidade); $i++) {
-            $char = stripos($validCharacters, $dscCidade[$i]);
-            if (!$char) {
-                $result = 1;
-            }
-
-            if ($dscCidade[$i] == " " && $dscCidade[$i + 1] == " ") {
-                $result = 2;
-            }
-        }
-
-        return $result;
-    }
-
-    public static function validateDescBairro($descBairro) {
-
-        $result = 0;
-        $validCharacters = '. abcdefghijklmnopqrstuvwxyzçãõáíóúàòìù';
-
-        for ($i = 0; $i < strlen($descBairro); $i++) {
-            $char = stripos($validCharacters, $descBairro[$i]);
-            if (!$char) {
-                $result = 1;
-            }
-
-            if ($descBairro[$i] == " " && $descBairro[$i + 1] == " ") {
-                $result = 2;
-            }
-        }
-
-        return $result;
-    }
-
+    
     public static function validateCodMunic($codMunic) {
 
         $result = FALSE;

1c3980430b855f87b758966dc0ee4409e642012f
MaxOliveira <maxwell_oliveira2@hotmail.com>
2013-10-13 20:37:10 -0300

    Criacao da pagina contatos

---
+++ View/Contato.php
@@ -0,0 +1,61 @@
+﻿<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <link rel="stylesheet" href="../view//shared/css/style.css" type="text/css">
+        <link rel="stylesheet" href="css/Contato.css" type="text/css">
+        <script type="text/javascript" src="../V.iew/shared/js/jquery.price_format.1.8.min.js"></script>
+        <link href="../shared/css/jquery-ui-1.10.3.custom.css" rel="stylesheet">
+        <title> Cadê Meu Hospital - Home</title>
+
+
+    </head>
+
+    <body>
+        <div class="root">                   
+            <?php require '../view/shared/header.php'; ?>
+
+            <?php require '../view/shared/navigation_bar.php'; ?>
+
+            <div align ="center">
+
+                <table border = "1">
+
+
+                    
+                    <b><font size="6">Equipe de Desenvolvimento</font></b>
+                    <table width="768"border="1">
+                        <tr>
+                            <td>Atilla Gallio </td>
+                            <td>atilla.gallio@gmail.com</td>
+                        </tr>
+                        <tr>
+                            <td>Euler Tiago </td>
+                            <td>tiago6654@gmail.com</td>
+                        </tr>
+                        <tr>
+                            <td>Levi Moraes</td>
+                            <td>levi.moraesds@gmail.com</td>
+                        </tr>
+                        <tr>
+                            <td>Maxwell de Oliveira</td>
+                            <td>maxwell_oliveira2@hotmail.com</td>
+                        </tr>
+                        <tr>
+                            <td>Paulo Tada</td>
+                            <td>paulohtfs@gmail.com</td>
+                        </tr>
+                    </table>
+                    <div class="content"> 
+
+
+                    </div>
+                    <br /><br /><br /><br /><br /><br /><br />
+                    <?php require '../view/shared/footer.php'; ?>
+            </div>
+
+
+        </div>
+    </body>
+</html>
--- View/Shared/navigation_bar.php
+++ View/Shared/navigation_bar.php
@@ -10,7 +10,7 @@
 
 			<li><a href="../index.php">Dados Abertos</a></li>
 
-			<li><a href="../index.php">Contatos</a></li>
+			<li><a href="../view/Contato.php">Contatos</a></li>
 
 		</ul>
 	</center>

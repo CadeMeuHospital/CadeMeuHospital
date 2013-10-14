9f86e5989876ae3eabccfe52d6651281220d5b01
paulohtfs <paulohtfs@gmail.com>
2013-10-14 17:19:08 -0300

    Teste da classe DataValidation

---
+++ Teste/Utils/DataValidationTest.php
@@ -0,0 +1,34 @@
+<?php
+
+class DataValidationTest extends PHPUnit_Framework_TestCase {
+
+    protected $dataValidation;
+
+    protected function setUp() {
+        $this->dataValidation = new DataValidation;
+    }
+
+    protected function tearDown() {
+    }
+
+    public function testThrowTextFieldException() {
+        $this->markTestIncomplete();
+    }
+
+    public function testThrowCodMunicException() {
+        $this->markTestIncomplete();
+    }
+
+    public function testValidateTextField() {
+        $this->markTestIncomplete();
+    }
+
+    public function testValidateCodMunic() {
+        $this->markTestIncomplete();
+    }
+
+    public function testValidateCodCNES() {
+        $this->markTestIncomplete();
+    }
+
+}

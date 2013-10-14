6cc551c407c10772fa5984b927ebd15167c352dd
paulohtfs <paulohtfs@gmail.com>
2013-10-14 19:15:44 -0300

    Evolução do teste ControllerProfileUBSTest.php

--- Teste/ControllerTest/ControllerProfileUBSTest.php
+++ Teste/ControllerTest/ControllerProfileUBSTest.php
@@ -17,16 +17,25 @@ class ControllerProfileUBSTest extends PHPUnit_Framework_TestCase {
     public function testSingleton() {
         $result = $this->controllerProfileUBS->getInstanceControllerProfileUBS();
         $this->assertInstanceOf('ControllerProfileUBS', $result);
+        $this->assertNotNULL($result);
     }
 
     public function testMakeObjectUBS(){
         $result = $this->controllerProfileUBS->makeObjectUBS(1, 456, 789, 123456, 224, "nomeUbs", "endereco", "bairro", "cidade", 12312345, "bom", "bom", "bom", "bom");
         $this->assertInstanceOf('ProfileUBS',$result);
+        $this->assertNotNULL($result);
     }
     
-    public function testSearchUBSByNameUBS(){
-        $result = $this->controllerProfileUBS->searchUBSByNameUBS("nomeUbs");
-        $this->assertInstanceOf('ProfileUBS',$result);
+    public function testSearchUBS(){
+        $result = $this->controllerProfileUBS->searchUBS("Taguatinga",2);
+    }
+    
+    public function testReturnUBS(){
+        $result = $this->controllerProfileUBS->returnUBS(1);
+        $idUBS = $result->getIdUBS();
+        $this->assertNotNULL($result);
+        $this->assertEquals(1,$idUBS);
+        
     }
 }
 

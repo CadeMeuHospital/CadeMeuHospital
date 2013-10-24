9be5c05ff25fa094c7c3967985b0a0141c79aaae
paulohtfs <paulohtfs@gmail.com>
2013-10-24 11:28:50 -0200

    Refatoração

--- Dao/ProfileUBSDAO.php
+++ Dao/ProfileUBSDAO.php
@@ -9,30 +9,38 @@ define('ID', 4);
 
 class ProfileUBSDAO {
 
-    public function __construct() {}
+    public function __construct() {
         
+    }
+
     public function searchUBSinDatabase($field, $searchType) {
+        
         if (!DataValidation::throwTextFieldException($field)) {
             throw new TextFieldException("Campo não pode ser nulo!");
-        } elseif(DataValidation::validateTextField($field) == 1) {
-		    throw new TextFieldException("Campo contém caracteres invalidos!");
-		} elseif(DataValidation::validateTextField($field) == 2) {
-			throw new TextFieldException("Campo contém espaços seguidos!");
-		} else {
-            switch ($searchType) {
-                case NOME :
-                    $sql = "SELECT * FROM ubs WHERE nom_estab LIKE '" . $field . "'";
-                    break;
-                case CIDADE :
-                    $sql = "SELECT * FROM ubs WHERE dsc_cidade LIKE '" . $field . "'";
-                    break;
-                case BAIRRO :
-                    $sql = "SELECT * FROM ubs WHERE dsc_bairro LIKE '" . $field . "'";
-                    break;
+        } else {
+            if (DataValidation::validateTextField($field) == 1) {
+                throw new TextFieldException("Campo contém caracteres invalidos!");
+            } else {
+                if (DataValidation::validateTextField($field) == 2) {
+                    throw new TextFieldException("Campo contém espaços seguidos!");
+                } else {
+                    switch ($searchType) {
+                        case NOME :
+                            $sql = "SELECT * FROM ubs WHERE nom_estab LIKE '" . $field . "'";
+                            break;
+                        case CIDADE :
+                            $sql = "SELECT * FROM ubs WHERE dsc_cidade LIKE '" . $field . "'";
+                            break;
+                        case BAIRRO :
+                            $sql = "SELECT * FROM ubs WHERE dsc_bairro LIKE '" . $field . "'";
+                            break;
+                    }
+                    $result = mysql_query($sql);
+                    return $result;
+                }
             }
-            $result = mysql_query($sql);
-            return $result;
         }
+        
     }
 
     public function returnUBS($id) {
@@ -41,6 +49,7 @@ class ProfileUBSDAO {
         $result = mysql_fetch_array($execute);
         return $result;
     }
+
 }
 
 ?>
--- Utils/DataValidation.php
+++ Utils/DataValidation.php
@@ -6,17 +6,23 @@ include_once '/../Utils/Exception/CodMunicException.php';
 define('SIZECODMUNIC', 6);
 
 class DataValidation {
+
     public static function throwTextFieldException($textField) {
 
         if (!DataValidation::validateNullFields($textField)) {
-			return FALSE;
-        } elseif(DataValidation::validateTextField($textField) == 1) {
-			return 1;
-        } elseif(DataValidation::validateTextField($textField) == 2) {
-			return 2;
-		} else {
-            return TRUE;
+            return FALSE;
+        } else {
+            if (DataValidation::validateTextField($textField) == 1) {
+                return 1;
+            } else {
+                if (DataValidation::validateTextField($textField) == 2) {
+                    return 2;
+                } else {
+                    return TRUE;
+                }
+            }
         }
+        
     }
 
     public static function throwCodMunicException($codMunic) {
@@ -32,19 +38,19 @@ class DataValidation {
     }
 
     public static function validateTextField($nome) {
-        
-            $caracteresValidos = '. abcdefghijklmnopqrstuvwxyzçãõáíóúàòìù';
-            
-            for ($i = 0; $i < strlen($nome); $i++) {
-                $char = stripos($caracteresValidos, $nome[$i]);
-                if(!$char){
-                    return 1;
-                }
-                
-                if($nome[$i] == " " && $nome[$i+1] == " "){
-                    return 2;   
-                }
+
+        $caracteresValidos = '. abcdefghijklmnopqrstuvwxyzçãõáíóúàòìù';
+
+        for ($i = 0; $i < strlen($nome); $i++) {
+            $char = stripos($caracteresValidos, $nome[$i]);
+            if (!$char) {
+                return 1;
             }
+
+            if ($nome[$i] == " " && $nome[$i + 1] == " ") {
+                return 2;
+            }
+        }
     }
 
     public static function validateCodMunic($codMunic) {
@@ -71,6 +77,7 @@ class DataValidation {
         }
         return $result;
     }
+
 }
 
 ?>

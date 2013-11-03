5e99286ff5741cab49677c56c6306d77be588823
EulerCarvalho <tiagobrasil2005@yahoo.com.br>
2013-11-03 13:30:03 -0200

    Alteração na view pra mostrar os botoes de avaliação

--- View/Profile.php
+++ View/Profile.php
@@ -1,64 +1,47 @@
-﻿
-
 <?php
 if (!isset($_GET['id'])) {
     header("location: ../index.php");
 }
 ?>
-
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
-
 <html>
-
-
     <head>
-
-
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
         <link rel="stylesheet" href="../view/shared/css/style.css" type="text/css">
         <link rel="stylesheet" href="css/home.css" type="text/css">
         <link rel="stylesheet" href="css/profile.css" type="text/css">
         <script type="text/javascript" src="../V.iew/shared/js/jquery.price_format.1.8.min.js"></script>
         <link href="../shared/css/jquery-ui-1.10.3.custom.css" rel="stylesheet">
-
-
         <title> Cadê Meu Hospital - Perfil UBS </title>
-
-
     </head>
-
+    
     <body>
-
-
         <div class="root">  
-
             <?php require '../view/shared/header.php'; ?>
-
             <?php require '../view/shared/navigation_bar.php'; ?>
             <?php
-            require_once '../Controller/ControllerProfileUBS.php';
+                require_once '../Controller/ControllerProfileUBS.php';
 
-            $controllerProfileUBS = ControllerProfileUBS::getInstanceControllerProfileUBS();
-
-            try {
-                $profileUBS = $controllerProfileUBS->returnUBS($_REQUEST['id']);
-            } catch (Exception $e) {
-                echo($e->getMessage("erro ao capturar id"));
-            }
+                $controllerProfileUBS = ControllerProfileUBS::getInstanceControllerProfileUBS();
+                
+                $idUBS=$_REQUEST['id'];
+                try {
+                    $profileUBS = $controllerProfileUBS->returnUBS($idUBS);
+                } catch (Exception $e) {
+                    echo($e->getMessage("erro ao capturar id"));
+                }
             ?>   
 
             <div class="profile"> 
 
-                <h2 style="text-indent:30px;">
-<?php echo $profileUBS->getNameUBS(); ?>
-                </h2>
+                <h2 style="text-indent:30px;"><?php echo $profileUBS->getNameUBS(); ?></h2>
                 <div class="content">
 
-                    <table id="tabela-dados-UBS" style="text-align: left; width:800px;">
-                        <tr>
+                    <table id="tabela-dados-UBS" style="text-align: left; width:700px;">
+                        <!--<tr>
                             <th>Nome:</th>
-                            <td class="align-left"><?php echo $profileUBS->getNameUBS(); ?></td>
-                        </tr>
+                            <td class="align-left"><?php //echo $profileUBS->getNameUBS(); ?></td>
+                        </tr>-->
                         <tr>
                             <th>Código do Municipio</th>
                             <td class="align-left"><?php echo $profileUBS->getCodMunic(); ?></td>
@@ -97,6 +80,25 @@ if (!isset($_GET['id'])) {
                     <br />
 
                 </div>
+            
+                <div class="evaluate">
+                    <h2>Avalie esta UBS!!</h2>
+                    <form name="Evaluate" action="EvaluateUBS.php" method="post">
+                        <table id="Tabela" >
+                            
+                                <th><input type="radio" name="evaluate" value="1" checked/> Ruim</th>
+                                <th><input type="radio" name="evaluate" value="2"/> Regular </th>        
+                                <th><input type="radio" name="evaluate" value="3"/> Bom </th>
+                                <th><input type="radio" name="evaluate" value="4"/> Muito Bom</th>
+                                <th><input type="radio" name="evaluate" value="5"/> Excelente! </th>
+                                                           
+                        </table>
+                        <input type="submit" name="submitEvaluate" value="Avaliar"/></tr>
+                        <input type="hidden" name="idUBS" value="<?php echo $idUBS; ?>" >
+                    </form>
+                </div>
+
+                <br><br>
 
                 <div id="mapholder"></div>
                 <h2>
@@ -128,6 +130,7 @@ if (!isset($_GET['id'])) {
                 <br /><br /><br /><br /><br /><br /><br />
 <?php require '../view/shared/footer.php'; ?>
             </div>
+        </div>
 
     </body>
 </html>

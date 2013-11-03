d29f91f23d0f8b1eebc24529038110c4591b1c1d
MaxOliveira <maxwell_oliveira2@hotmail.com>
2013-11-03 12:47:57 -0200

    Refatoracao do teste

--- Teste/ControllerTest/ControllerProfileUBSTest.php
+++ Teste/ControllerTest/ControllerProfileUBSTest.php
@@ -5,38 +5,53 @@ require_once dirname(__FILE__) . '/../../Controller/ControllerProfileUBS.php';
 class ControllerProfileUBSTest extends PHPUnit_Framework_TestCase {
 
     protected $controllerProfileUBS;
+    protected $objectProfileUBS;
 
-    protected function setUp() {
+    protected function setUpControllerProfileUBS() {
         $this->controllerProfileUBS = ControllerProfileUBS::getInstanceControllerProfileUBS();
     }
-
-    protected function tearDown() {
-        unset($this->controllerProfileUBS);
+    protected function setUpObjectProfileUBS(){
+     $this->objectProfileUBS = $this->controllerProfileUBS->makeObjectUBS(1, 456, 789, 123456, 224, "nomeUbs", "endereco", "bairro", "cidade", 12312345, "bom", "bom", "bom", "bom");  
     }
 
-    public function testSingleton() {
+    protected function tearDownControllerProfileUBS() {
+        unset($this->controllerProfileUBS);
+    }
+    protected function tearDownObjectProfileUBS(){
+        unset($this->objectProfileUBS);
+    }
+
+    public function testSingletonNotNull() {
         $result = $this->controllerProfileUBS->getInstanceControllerProfileUBS();
         $this->assertInstanceOf('ControllerProfileUBS', $result);
         $this->assertNotNULL($result);
     }
 
-    public function testMakeObjectUBS(){
-        $result = $this->controllerProfileUBS->makeObjectUBS(1, 456, 789, 123456, 224, "nomeUbs", "endereco", "bairro", "cidade", 12312345, "bom", "bom", "bom", "bom");
-        $this->assertInstanceOf('ProfileUBS',$result);
+    public function testMakeObjectUBSNotNull() {
+        $result = $this->objectProfileUBS;
         $this->assertNotNULL($result);
     }
-    
-    public function testSearchUBS(){
-        $result = $this->controllerProfileUBS->searchUBS("Taguatinga",2);
+
+    public function testMakeObjectUBSInstanceOf() {
+        $result = $this->objectProfileUBS;
+        $this->assertInstanceOf('ProfileUBS', $result);
     }
-    
-    public function testReturnUBS(){
+
+    public function testSearchUBS() {
+        $result = $this->controllerProfileUBS->searchUBS("Taguatinga", 2);
+    }
+
+    public function testReturnUBSNotNull() {
         $result = $this->controllerProfileUBS->returnUBS(1);
         $idUBS = $result->getIdUBS();
         $this->assertNotNULL($result);
-        $this->assertEquals(1,$idUBS);
-        
     }
-}
 
+    public function testReturnUBSEqualsID() {
+        $result = $this->controllerProfileUBS->returnUBS(1);
+        $idUBS = $result->getIdUBS();
+        $this->assertEquals(1, $idUBS);
+    }
+
+}
 ?>

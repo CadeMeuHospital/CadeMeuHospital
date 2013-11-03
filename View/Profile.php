91131bdb55eb05c0636fe3262f337218f75902d1
MaxOliveira <maxwell_oliveira2@hotmail.com>
2013-11-03 10:16:35 -0200

    Sistema de comentarios Facebook

--- View/Profile.php
+++ View/Profile.php
@@ -111,6 +111,17 @@ if (!isset($_GET['id'])) {
                             document.getElementById("mapholder").innerHTML = "<img src='" + img_url + "'>";
                         }
                     </script>
+                    <script>(function(d, s, id) {
+                        var js, fjs = d.getElementsByTagName(s)[0];
+                        if (d.getElementById(id))
+                            return;
+                        js = d.createElement(s);
+                        js.id = id;
+                        js.src = "//connect.facebook.net/pt_BR/all.js#xfbml=1";
+                        fjs.parentNode.insertBefore(js, fjs);
+                    }(document, 'script', 'facebook-jssdk'));</script>
+                <div id="fb-root"></div>
+                <div class="fb-comments" data-href="http://localhost/CadeMeuHospital/view/Profile.php?id=<?php echo $_REQUEST['id']?>" data-numposts="4" data-width="600"></div>
                 </h2>
 
 

f2957e3dae5d2f4cc32a142cc7f536e0dfe42fc3
EulerCarvalho <tiagobrasil2005@yahoo.com.br>
2013-11-04 09:59:49 -0200

    evolução dos testes

--- Teste/Dao/ProfileUBSDAOTest.php
+++ Teste/Dao/ProfileUBSDAOTest.php
@@ -2,6 +2,8 @@
 
 require_once dirname(__FILE__) . '/../../Dao/ProfileUBSDAO.php';
 
+define("IDEVALUATE", "999999");
+
 class ProfileUBSDAOTest extends PHPUnit_Framework_TestCase {
 
     protected $profileUBSDao;
@@ -12,6 +14,17 @@ class ProfileUBSDAOTest extends PHPUnit_Framework_TestCase {
 
     protected function tearDownProfileUBS() {
         unset($this->profileUBSDao);
+    }
+    
+    protected function tearDownDataBase(){
+        $sqlDeleteRowEvaluate = "DELETE FROM evaluate WHERE id_evaluate LIKE 999999";
+        $result = mysql_query($sqlDeleteRowEvaluate);
+    }
+
+    protected function insertUBSInDataBase() {
+        $queryInsertUBS = "INSERT INTO ubs (cod_unico, vlr_latitude, vlr_longitude, cod_munic, cod_cnes, nom_estab, dsc_endereco, dsc_bairro, dsc_cidade, dsc_telefone, dsc_estrut_fisic_ambiencia, dsc_adap_defic_fisic_idosos, dsc_equipamentos, dsc_medicamentos, average) VALUES (37798, -99.9999999999999, -99.9999999999999, 999999, 99999, 'testeNome','testeEndereco', 'testeBairro', 'descBairro', 99999999999,  'testeDescEstFisiAmb', 'testDescAdaptIdoso', 'testeDescEquip', 'testeDescEquip', 9999999)";
+        $executeQuery = mysql_query($queryInsertUBS);
+        return $executeQuery;
     }
 
     public function testSearchUBSinDatabase() {
@@ -25,28 +38,36 @@ class ProfileUBSDAOTest extends PHPUnit_Framework_TestCase {
         $this->setUpProfileUBS();
         $resultFalse = $this->profileUBSDao->returnUBS(40000);
         $this->assertFalse($resultFalse);
-        $this->tearDownProfileUBS();        
+        $this->tearDownProfileUBS();
     }
-    
-    public function testSearchUBSInTableEvaluate(){
+
+    public function testSearchUBSInTableEvaluate() {
         $this->setUpProfileUBS();
         $resultNotNULL = $this->profileUBSDao->searchUBSInTableEvaluate(1);
         $this->assertNotNULL($resultNotNULL);
         $this->tearDownProfileUBS();
-    
     }
-    
-    public function testSaveEvaluationUBSNotNULL(){
+
+    public function testeSaveEvaluationUBSInsere() {
         $this->setUpProfileUBS();
-        $resultNotNULL = $this->profileUBSDao->saveEvaluationUBS(6, 1);
-        $this->assertNotNULL($resultNotNULL);
-        $this->tearDownProfileUBS();        
+        $result = $this->profileUBSDao->saveEvaluationUBS(NULL, IDEVALUATE);
+        $this->tearDownProfileUBS();
+        $this->tearDownDataBase();
     }
-    
-    public function testTakeAverageUBSFalse(){
+
+    public function testSaveEvaluationUBSNotNULL() {
+        $this->setUpProfileUBS();
+        $resultNotNULL = $this->profileUBSDao->saveEvaluationUBS(5, IDEVALUATE);
+        $this->assertNotNULL($resultNotNULL);
+        $this->tearDownProfileUBS();
+        $this->tearDownDataBase();
+    }
+
+    public function testTakeAverageUBSFalse() {
         $this->setUpProfileUBS();
         $resultFalse = $this->profileUBSDao->takeAverageUBS(NULL);
         $this->assertFalse($resultFalse);
         $this->tearDownProfileUBS();
     }
+
 }

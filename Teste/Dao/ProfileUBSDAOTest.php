e76d10a67ed13d8b4605a7a5f949c0c417e42b58
paulohtfs <paulohtfs@gmail.com>
2013-11-04 22:57:55 -0200

    Evolução do teste ProfileUBSDAOTest

--- Teste/Dao/ProfileUBSDAOTest.php
+++ Teste/Dao/ProfileUBSDAOTest.php
@@ -8,17 +8,19 @@ class ProfileUBSDAOTest extends PHPUnit_Framework_TestCase {
 
     protected $profileUBSDao;
 
-    /*SetUp methods to inicialize the test suit */
-    protected function setUp(){
+    /* SetUp methods to inicialize the test suit */
+
+    protected function setUp() {
         $this->setUpProfileUBS();
     }
-    
+
     protected function setUpProfileUBS() {
         $this->profileUBSDao = new ProfileUBSDAO();
     }
-    
-    /*TearDown methods to delete the suit case */
-    protected function tearDown(){
+
+    /* TearDown methods to delete the suit case */
+
+    protected function tearDown() {
         $this->tearDownDataBase();
         $this->tearDownProfileUBS();
     }
@@ -26,40 +28,46 @@ class ProfileUBSDAOTest extends PHPUnit_Framework_TestCase {
     protected function tearDownProfileUBS() {
         unset($this->profileUBSDao);
     }
-    
-    protected function tearDownDataBase(){
-        $sqlDeleteRowEvaluate = "DELETE FROM evaluate WHERE id_evaluate LIKE 999999";
+
+    protected function tearDownDataBase() {
+        $sqlDeleteRowEvaluate = "DELETE FROM evaluate WHERE id_cod_unico='999999'";
         mysql_query($sqlDeleteRowEvaluate);
     }
 
-    /*Method insertUBSInDatabase suit test case*/
+    /* Method insertUBSInDatabase suit test case */
+
     protected function insertUBSInDataBase() {
         $queryInsertUBS = "INSERT INTO ubs (cod_unico, vlr_latitude, vlr_longitude, cod_munic, cod_cnes, nom_estab, dsc_endereco, dsc_bairro, dsc_cidade, dsc_telefone, dsc_estrut_fisic_ambiencia, dsc_adap_defic_fisic_idosos, dsc_equipamentos, dsc_medicamentos, average) VALUES (37798, -99.9999999999999, -99.9999999999999, 999999, 99999, 'testeNome','testeEndereco', 'testeBairro', 'descBairro', 99999999999,  'testeDescEstFisiAmb', 'testDescAdaptIdoso', 'testeDescEquip', 'testeDescEquip', 9999999)";
         $executeQuery = mysql_query($queryInsertUBS);
         return $executeQuery;
     }
 
-    /*Method searchUBSinDatabase suit test case*/
+    /* Method searchUBSinDatabase suit test case */
+
     public function testSearchUBSinDatabase() {
         $resultNotNULL = $this->profileUBSDao->searchUBSinDatabase("taguatinga", 2);
         $this->assertNotNULL($resultNotNULL);
     }
 
-    /*Method returnUBS suit test case*/
+    /* Method returnUBS suit test case */
+
     public function testReturnUBSFalse() {
         $resultFalse = $this->profileUBSDao->returnUBS(40000);
         $this->assertFalse($resultFalse);
     }
 
-    /*Method searchUBSInTableEvaluate suit test case*/
+    /* Method searchUBSInTableEvaluate suit test case */
+
     public function testSearchUBSInTableEvaluateNotNull() {
         $resultNotNULL = $this->profileUBSDao->searchUBSInTableEvaluate(1);
         $this->assertNotNull($resultNotNULL);
     }
 
-    /*Method saveEvaluationUBS suit test case*/
+    /* Method saveEvaluationUBS suit test case */
+
     public function testeSaveEvaluationUBSInsert() {
-        $result = $this->profileUBSDao->saveEvaluationUBS(NULL, IDEVALUATE);
+        $resultNotNull = $this->profileUBSDao->saveEvaluationUBS(NULL, IDEVALUATE);
+        $this->assertNotNull($resultNotNull);
     }
 
     public function testSaveEvaluationUBSNotNULL() {
@@ -67,10 +75,16 @@ class ProfileUBSDAOTest extends PHPUnit_Framework_TestCase {
         $this->assertNotNULL($resultNotNULL);
     }
 
-    /*Method takeAvarageUBS suit test case*/
+    /* Method takeAvarageUBS suit test case */
+
     public function testTakeAverageUBSFalse() {
         $resultFalse = $this->profileUBSDao->takeAverageUBS(NULL);
         $this->assertFalse($resultFalse);
+    }
+
+    public function testTakeAverageUBSNotNull() {
+        $resultNotNull = $this->profileUBSDao->takeAverageUBS(1);
+        $this->assertNotNull($resultNotNull);
     }
 
 }

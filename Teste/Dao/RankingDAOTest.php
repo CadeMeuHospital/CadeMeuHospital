2c5690d6a500e228d63d6c8d766d87efd6d3d00d
paulohtfs <paulohtfs@gmail.com>
2013-11-06 14:46:15 -0200

    Correção do teste RankingDAOTest

--- Teste/Dao/RankingDAOTest.php
+++ Teste/Dao/RankingDAOTest.php
@@ -5,9 +5,17 @@ include_once dirname(__FILE__) . "/../../Dao/RankingDAO.php";
 class RankingDAOTest extends PHPUnit_Framework_TestCase {
 
     protected $object;
+    
+    protected function setUp(){
+        $this->setUpRankingDAO();
+    }
 
     protected function setUpRankingDAO() {
         $this->object = new RankingDAO();
+    }
+    
+    protected function tearDown(){
+        $this->tearDownRankingDAO();
     }
 
     protected function tearDownRankingDAO() {
@@ -15,25 +23,17 @@ class RankingDAOTest extends PHPUnit_Framework_TestCase {
     }
 
     public function testGetRank() {
-        $this->setUpRankingDAO();
         $result = $this->object->getRank("US OSWALDO DE SOUZA", 1, 3);
-        $this->tearDownRankingDAO();
     }
     public function testGetRankFalse() {
-        $this->setUpRankingDAO();
         $resultFalse = $this->object->getRank("dnfjsf", 9999999, 123);
-        $this->tearDownRankingDAO();
     }
 
     public function testGetRankByCityNotNull() {
-        $this->setUpRankingDAO();
         $result = $this->object->getRankByCity("Aracaju");
-        $this->tearDownRankingDAO();
     }
 
     public function testGetRankByCityFalse() {
-        $this->setUpRankingDAO();
         $resultFalse = $this->object->getRankByCity("adjaijaid");
-        $this->tearDownRankingDAO();
     }
 }
--- Teste/Model/ProfileUBSTest.php
+++ Teste/Model/ProfileUBSTest.php
@@ -5,9 +5,17 @@ require_once dirname(__FILE__) . '/../../Model/profileUBS.php';
 class ProfileUBSTest extends PHPUnit_Framework_TestCase {
 
     protected $profileUBS;
+    
+    protected function setUp(){
+        $this->setUpProfileUBS();
+    }
 
     protected function setUpProfileUBS() {
         $this->profileUBS = new profileUBS(1, 456, 789, 123456, 224, "nomeUbs", "endereco", "bairro", "cidade", 12312345, "bom", "bom", "bom", "bom");
+    }
+    
+    protected function tearDown(){
+        $this->tearDownProfileUBS();
     }
 
     protected function tearDownProfileUBS() {
@@ -15,76 +23,61 @@ class ProfileUBSTest extends PHPUnit_Framework_TestCase {
     }
 
     public function testGetIdUBS() {
-        $this->setUpProfileUBS();
-        $this->assertEquals(1, $this->profileUBS->getIdUBS());
-        $this->tearDownProfileUBS();
+        $resultEquals = $this->profileUBS->getIdUBS();
+        $this->assertEquals(1, $resultEquals);
     }
     public function testGetLatitudeUBS(){
-        $this->setUpProfileUBS();
-        $this->assertEquals(456, $this->profileUBS->getLatitudeUBS());
-        $this->tearDownProfileUBS();
+        $resultEquals = $this->profileUBS->getLatitudeUBS();
+        $this->assertEquals(456, $resultEquals);
     }
     public function testGetLongitudeUBS(){
-        $this->setUpProfileUBS();
-        $this->assertEquals(789, $this->profileUBS->getLongitudeUBS());
-        $this->tearDownProfileUBS();
+        $resultEquals = $this->profileUBS->getLongitudeUBS();
+        $this->assertEquals(789, $resultEquals);
     }
     public function testGetCodMunic(){
-        $this->setUpProfileUBS();
-        $this->assertEquals(123456, $this->profileUBS->getCodMunic());
-        $this->tearDownProfileUBS();
+        $resultEquals = $this->profileUBS->getCodMunic();
+        $this->assertEquals(123456, $resultEquals);
     }
     public function testGetCodCNES(){
-        $this->setUpProfileUBS();
-        $this->assertEquals(224, $this->profileUBS->getCodCNES());
-        $this->tearDownProfileUBS();
+        $resultEquals = $this->profileUBS->getCodCNES();
+        $this->assertEquals(224, $resultEquals);
     }
     public function testGetNameUBS(){
-        $this->setUpProfileUBS();
-        $this->assertEquals("nomeUbs", $this->profileUBS->getNameUBS());
-        $this->tearDownProfileUBS();
+        $resultEquals = $this->profileUBS->getNameUBS();
+        $this->assertEquals("nomeUbs", $resultEquals);
     }
     public function testGetDescEnder(){
-        $this->setUpProfileUBS();
-        $this->assertEquals("endereco", $this->profileUBS->getDescEnder());
-        $this->tearDownProfileUBS();
+        $resultEquals = $this->profileUBS->getDescEnder();
+        $this->assertEquals("endereco", $resultEquals);
     }
     public function testGetDscCidade(){
-        $this->setUpProfileUBS();
-        $this->assertEquals("cidade", $this->profileUBS->getDscCidade());
-        $this->tearDownProfileUBS();
+        $resultEquals = $this->profileUBS->getDscCidade();
+        $this->assertEquals("cidade", $resultEquals);
     }
     public function testGetDescBairro(){
-        $this->setUpProfileUBS();
-        $this->assertEquals("bairro", $this->profileUBS->getDescBairro());
-        $this->tearDownProfileUBS();
+        $resultEquals = $this->profileUBS->getDescBairro();
+        $this->assertEquals("bairro", $resultEquals);
     }
     public function testGetPhoneUBS(){
-        $this->setUpProfileUBS();
-        $this->assertEquals(12312345, $this->profileUBS->getPhoneUBS());
-        $this->tearDownProfileUBS();
+        $resultEquals = $this->profileUBS->getPhoneUBS();
+        $this->assertEquals(12312345, $resultEquals);
     }
     public function testGetPhysicStructure(){
-        $this->setUpProfileUBS();
-        $this->assertEquals("bom", $this->profileUBS->getPhysicStructureUBS());
-        $this->tearDownProfileUBS();
+        $resultEquals = $this->profileUBS->getPhysicStructureUBS();
+        $this->assertEquals("bom", $resultEquals);
     }
     public function testGetAdapOldPeople(){
-        $this->setUpProfileUBS();
-        $this->assertEquals("bom", $this->profileUBS->getAdapOldPeople());
-        $this->tearDownProfileUBS();
+        $resultEquals = $this->profileUBS->getAdapOldPeople();
+        $this->assertEquals("bom", $resultEquals);
     }
     public function testGetDescriTools(){
-        $this->setUpProfileUBS();
-        $this->assertEquals("bom", $this->profileUBS->getDescriTools());
-        $this->tearDownProfileUBS();
+        $resultEquals = $this->profileUBS->getDescriTools();
+        $this->assertEquals("bom", $resultEquals);
     }
     public function testGetDescMedicine(){
-        $this->setUpProfileUBS();
-        $this->assertEquals("bom", $this->profileUBS->getDescMedicine());
-        $this->tearDownProfileUBS();
+        $resultEquals = $this->profileUBS->getDescMedicine();
+        $this->assertEquals("bom", $resultEquals);
     }
-
 }
 
 ?>

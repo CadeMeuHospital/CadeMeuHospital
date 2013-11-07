cb0c589f24d2507b93b6576d3f72942a010bda6d
MaxOliveira <maxwell_oliveira2@hotmail.com>
2013-11-07 14:46:40 -0200

    Finalizacao do ranking

--- View/Home.php
+++ View/Home.php
@@ -1,9 +1,11 @@
-<?php       
-    if (!isset($_POST['submit'])) {
-        $rankType = "geral";
-    }else{
-        $rankType = $_POST['rank'];
-    }
+<?php
+if (!isset($_POST['submit'])) {
+    $rankType = "geral";
+} else {
+    $rankType = $_POST['rank'];
+    $textField = $_POST['textField'];
+}
+$mensagemErro = "Desculpe-nos! Não há UBS avaliadas neste local! D=";
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 
@@ -19,10 +21,10 @@
 
     <body align="center">
         <div class="root">                   
-            <?php 
-                require '../view/shared/header.php';
-                require '../view/shared/navigation_bar.php';
-                require '../Controller/ControllerRanking.php'; 
+            <?php
+            require '../view/shared/header.php';
+            require '../view/shared/navigation_bar.php';
+            require '../Controller/ControllerRanking.php';
             ?>            
 
             <div id="center">
@@ -33,40 +35,47 @@
                         <input type="radio" name="rank" value="geral" checked/> Geral <br>
                         <input type="radio" name="rank" value="cidade"/> Cidade <br>
                         <input type="radio" name="rank" value="bairro"/> Bairro <br>
+                        <input type="text" name ="textField" />
                         <input type="submit" name="submit" value="Enviar"/>
                     </form>
                     <br><br><br><br><br><br>
-                    <?php
-                        
-                        $controllerRanking = ControllerRanking::getInstanceControllerRanking();
-                        switch($rankType){
-                            case "geral":
-                                $topFiveUBS = $controllerRanking->makeRank();
-                                break;
-                            case "cidade":
-                                $topFiveUBS = $controllerRanking->makeRankByCity($rankType);
-                                break;
-                            case "bairro":
-                                $topFiveUBS = $controllerRanking->makeRankByNeighborhood($rankType);
-                                break;
-                        }
-                        $topFiveUBS = $controllerRanking->makeRank();
-                        for ($i=0;$i<  mysql_num_rows($topFiveUBS);$i++){
-                              $nameUBS = mysql_result($topFiveUBS,$i,"nom_estab" );
-                              $idUBS = mysql_result($topFiveUBS,$i,"cod_unico" );
-                              $average = mysql_result($topFiveUBS,$i,"average" );
 
-                              $path = "../view/Profile.php?id=" . $idUBS . "";
-                              echo "<a href=" . $path . "> " . $nameUBS . " </a> - ".$average."<br>";
+                    <?php
+                    $controllerRanking = ControllerRanking::getInstanceControllerRanking();
+                    switch ($rankType) {
+                        case "geral":
+                            $topFiveUBS = $controllerRanking->makeRank();
+                            break;
+                        case "cidade":
+                            $topFiveUBS = $controllerRanking->makeRankByCity($textField);
+                            break;
+                        case "bairro":
+                            $topFiveUBS = $controllerRanking->makeRankByNeighborhood($textField);
+                            break;
+                    }
+                    $numberUBS = mysql_num_rows($topFiveUBS);
+                    if ($numberUBS == 0){
+                        print"  <script>
+                                    alert('" .$mensagemErro . "')
+                                    window.location='Home.php';
+                                </script> ";
+                    }else{
+                        for ($i = 0; $i < $numberUBS; $i++) {
+                            $nameUBS = mysql_result($topFiveUBS, $i, "nom_estab");
+                            $idUBS = mysql_result($topFiveUBS, $i, "cod_unico");
+                            $average = mysql_result($topFiveUBS, $i, "average");
+
+                            $path = "../view/Profile.php?id=" . $idUBS . "";
+                            echo "<a href=" . $path . "> " . $nameUBS . " </a> - " . $average . "<br>";
                         }
-                         
+                    }
                     ?>
-                    
+
                 </div>        
             </div>
 
 
-            <?php require '../view/shared/footer.php'; ?>
+<?php require '../view/shared/footer.php'; ?>
         </div>
 
 
@@ -75,5 +84,4 @@
 </html>
 
 <?php
-
 ?>

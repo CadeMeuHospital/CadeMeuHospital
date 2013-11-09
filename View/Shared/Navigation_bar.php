690eaaf2f428998524132e7a668dc3ea7e2712b7
jgbs <joaogabrieldebrittoesilva@gmail.com>
2013-11-09 01:23:16 -0200

    pequena refatoracao

--- View/Shared/Navigation_bar.php
+++ View/Shared/Navigation_bar.php
@@ -4,9 +4,7 @@
             <ul>
                 <li><a href="../index.php">Home</a></li>
 
-                <!--<li><a href="../index.php">UBS</a></li>-->
-
-                <li><a href="../View/Map.php">Mapa</a></li>
+                <li><a href="../View/ClosestUBS.php">UBS mais próxima a mim!</a></li>
 
                 <li><a href="http://dados.gov.br/dataset/unidades-basicas-de-saude-ubs" target =" _blank">Dados Abertos</a></li>
 
@@ -15,7 +13,4 @@
             </ul>
         </center>
     </div>
-
-
-
 </div>

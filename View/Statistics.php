ee497a94c0612722449ad99ab2b07fbe96c58890
levimoraes <levi.moraes@yahoo.com.br>
2013-11-10 17:02:22 -0200

    Refatoracao Api Google Charts - Levi & Atilla

--- View/Statistics.php
+++ View/Statistics.php
@@ -97,96 +97,96 @@
                             data.setValue(1, 0, 'Rio Grande do Norte');
                             data.setValue(1, 1, 300);
 
-data.setValue(2, 0, 'Alagoas'); 
-  data.setValue(2, 1, 100);
-  
-  data.setValue(3, 0, 'Amapa'); 
-  data.setValue(3, 1, 100);
-  
-  data.setValue(4, 0, 'Acre'); 
-  data.setValue(4, 1, 100);
-  
-  data.setValue(5, 0, 'Amazonas'); 
-  data.setValue(5, 1, 100);
-  
-  data.setValue(6, 0, 'Bahia'); 
-  data.setValue(6, 1, 100);
+                            data.setValue(2, 0, 'Alagoas');
+                            data.setValue(2, 1, 100);
 
-  data.setValue(7, 0, 'Ceara'); 
-  data.setValue(7, 1, 100);
-  
- data.setValue(8, 0, 'Distrito Federal'); 
-  data.setValue(8, 1, 600);
-  
-  data.setValue(9, 0, 'Espirito Santo'); 
-  data.setValue(9, 1, 100);
-  
-  data.setValue(10, 0, 'Goias'); 
-  data.setValue(10, 1, 100);
-  
-  data.setValue(11, 0, 'Maranhao'); 
-  data.setValue(11, 1, 100);
-  
-  data.setValue(12, 0, 'Minas Gerais'); 
-  data.setValue(12, 1, 100);
-  
-  data.setValue(13, 0, 'Mato Grosso'); 
-  data.setValue(13, 1, 100); 
-  
-data.setValue(14, 0, 'Mato Grosso do Sul'); 
-  data.setValue(14, 1, 100);
-  
-  data.setValue(15, 0, 'Maranhao'); 
-  data.setValue(15, 1, 100);
-  
-  data.setValue(16, 0, 'Parana'); 
-  data.setValue(16, 1, 100);
-  
-  data.setValue(17, 0, 'Pernambuco'); 
-  data.setValue(17, 1, 100);
-  
-  data.setValue(25, 0, 'Piaui'); 
-  data.setValue(25, 1, 100);  
-  
-  data.setValue(26, 0, 'Para'); 
-  data.setValue(26, 1, 100);
-  
-  data.setValue(27, 0, 'Paraiba'); 
-  data.setValue(27, 1, 100);
-     
-  data.setValue(18, 0, 'Roraima'); 
-  data.setValue(18, 1, 100); 
-  
-  data.setValue(19, 0, 'Rondonia'); 
-  data.setValue(19, 1, 100);
-  
-  data.setValue(20, 0, 'Rio de Janeiro'); 
-  data.setValue(20, 1, 100);
-  
-  data.setValue(21, 0,'Sao Paulo'); 
-  data.setValue(21, 1, 100);
-  
-  data.setValue(22, 0, 'Santa Catarina'); 
-  data.setValue(22, 1, 100);
-  
-  data.setValue(23, 0, 'Sergipe'); 
-  data.setValue(23, 1, 100);
-  
-  data.setValue(24, 0, 'Tocantins'); 
-  data.setValue(24, 1, 100);
-  
-  var options = {}; 
-      options['region'] = 'BR'; 
-      options['resolution'] = 'provinces'; 
-      options['width'] = 556; 
-      options['height'] = 347;
-      options['colorAxis'] = {colors : ['#E5DEE2','#990000']};
+                            data.setValue(3, 0, 'Amapa');
+                            data.setValue(3, 1, 100);
+
+                            data.setValue(4, 0, 'Acre');
+                            data.setValue(4, 1, 100);
+
+                            data.setValue(5, 0, 'Amazonas');
+                            data.setValue(5, 1, 100);
+
+                            data.setValue(6, 0, 'Bahia');
+                            data.setValue(6, 1, 100);
+
+                            data.setValue(7, 0, 'Ceara');
+                            data.setValue(7, 1, 100);
+
+                            data.setValue(8, 0, 'Distrito Federal');
+                            data.setValue(8, 1, 600);
+
+                            data.setValue(9, 0, 'Espirito Santo');
+                            data.setValue(9, 1, 100);
+
+                            data.setValue(10, 0, 'Goias');
+                            data.setValue(10, 1, 100);
+
+                            data.setValue(11, 0, 'Maranhao');
+                            data.setValue(11, 1, 100);
+
+                            data.setValue(12, 0, 'Minas Gerais');
+                            data.setValue(12, 1, 100);
+
+                            data.setValue(13, 0, 'Mato Grosso');
+                            data.setValue(13, 1, 100);
+
+                            data.setValue(14, 0, 'Mato Grosso do Sul');
+                            data.setValue(14, 1, 100);
+
+                            data.setValue(15, 0, 'Maranhao');
+                            data.setValue(15, 1, 100);
+
+                            data.setValue(16, 0, 'Parana');
+                            data.setValue(16, 1, 100);
+
+                            data.setValue(17, 0, 'Pernambuco');
+                            data.setValue(17, 1, 100);
+
+                            data.setValue(25, 0, 'Piaui');
+                            data.setValue(25, 1, 100);
+
+                            data.setValue(26, 0, 'Para');
+                            data.setValue(26, 1, 100);
+
+                            data.setValue(27, 0, 'Paraiba');
+                            data.setValue(27, 1, 100);
+
+                            data.setValue(18, 0, 'Roraima');
+                            data.setValue(18, 1, 100);
+
+                            data.setValue(19, 0, 'Rondonia');
+                            data.setValue(19, 1, 100);
+
+                            data.setValue(20, 0, 'Rio de Janeiro');
+                            data.setValue(20, 1, 100);
+
+                            data.setValue(21, 0, 'Sao Paulo');
+                            data.setValue(21, 1, 100);
+
+                            data.setValue(22, 0, 'Santa Catarina');
+                            data.setValue(22, 1, 100);
+
+                            data.setValue(23, 0, 'Sergipe');
+                            data.setValue(23, 1, 100);
+
+                            data.setValue(24, 0, 'Tocantins');
+                            data.setValue(24, 1, 100);
+
+                            var options = {};
+                            options['region'] = 'BR';
+                            options['resolution'] = 'provinces';
+                            options['width'] = 556;
+                            options['height'] = 347;
+                            options['colorAxis'] = {colors: ['#E5DEE2', '#990000']};
 
 
-  var geochart = new google.visualization.GeoChart( 
-      document.getElementById('visualization')); 
-  geochart.draw(data,options); 
-} 
+                            var geochart = new google.visualization.GeoChart(
+                                    document.getElementById('visualization'));
+                            geochart.draw(data, options);
+                        }
                     </script>
                     <!--Div that will hold the dashboard-->
                     <div id="dashboard_div">

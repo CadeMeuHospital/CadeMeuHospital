e5f85b73f2e38ec91edf6dea0923145a21d4cc1a
levimoraes <levi.moraes@yahoo.com.br>
2013-11-10 17:51:11 -0200

    Testes Statics

--- Teste/Dao/StatisticsDAOTest.php
+++ Teste/Dao/StatisticsDAOTest.php
@@ -1,60 +1,32 @@
 <?php
 
 require_once dirname(__FILE__) . '/../../Dao/StatisticsDAO.php';
+
 class StatisticsDAOTest extends PHPUnit_Framework_TestCase {
 
-    /**
-     * @var StatisticsDAO
-     */
-    protected $object;
+    protected $statisticsDao;
 
-    /**
-     * Sets up the fixture, for example, opens a network connection.
-     * This method is called before a test is executed.
-     */
     protected function setUp() {
-        $this->object = StatisticsDAO::getInstanceStatisticsDAO();
+        $this->statisticsDao = StatisticsDAO::getInstanceStatisticsDAO();
     }
 
-    /**
-     * Tears down the fixture, for example, closes a network connection.
-     * This method is called after a test is executed.
-     */
     protected function tearDown() {
-        
+        unset($this->statisticsDao);
     }
 
-    /**
-     * @covers StatisticsDAO::getInstanceStatisticsDAO
-     * @todo   Implement testGetInstanceStatisticsDAO().
-     */
     public function testGetInstanceStatisticsDAO() {
-        // Remove the following lines when you implement this test.
-        $this->markTestIncomplete(
-                'This test has not been implemented yet.'
-        );
+        $resultNotNull = $this->statisticsDao->getValuesToChartAverageEvaluate();
+        $this->assertNotNull($resultNotNull);        
     }
 
-    /**
-     * @covers StatisticsDAO::getValuesToChartAverageEvaluate
-     * @todo   Implement testGetValuesToChartAverageEvaluate().
-     */
     public function testGetValuesToChartAverageEvaluate() {
-        // Remove the following lines when you implement this test.
-        $this->markTestIncomplete(
-                'This test has not been implemented yet.'
-        );
+       
+       
     }
 
-    /**
-     * @covers StatisticsDAO::getValuesToChartAverageEvaluateSigleUBS
-     * @todo   Implement testGetValuesToChartAverageEvaluateSigleUBS().
-     */
+  
     public function testGetValuesToChartAverageEvaluateSigleUBS() {
-        // Remove the following lines when you implement this test.
-        $this->markTestIncomplete(
-                'This test has not been implemented yet.'
-        );
+      
     }
 
 }

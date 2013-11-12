8328328275e83684ab91c46dd9a611c9e270698e
levimoraes <levi.moraes@yahoo.com.br>
2013-11-12 13:25:11 -0200

    TDD - Criacao do  testgetCity

---
+++ Teste/Model/UserTest.php
@@ -0,0 +1,26 @@
+<?php
+
+require_once dirname(__FILE__) . '/../../Model/User.php';
+
+class UserTest extends PHPUnit_Framework_TestCase {
+
+    protected $user;
+    
+    protected function setUp(){
+        $this->setUpUser();
+    }
+    
+    protected function setUpUser(){
+        $this->user = new User("Aracaju");
+    }
+    
+    protected function tearDown(){
+        $this->tearDownUser();
+    }
+    
+    public function testgetCity() {
+        $resultEquals = $this->user->getCity();
+        $this->assertEquals("Aracaju", $resultEquals);
+    }
+}
+?>

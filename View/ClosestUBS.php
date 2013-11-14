cd4335912df55e247e6b5ed0bc7747a3f589e558
Larissa <Larissa@Larissa-PC>
2013-11-14 10:30:09 -0200

    Arrumando a identação de algumas classes

--- View/ClosestUBS.php
+++ View/ClosestUBS.php
@@ -1,6 +1,6 @@
 <?php
-    require_once '../Controller/ControllerProfileUBS.php';
-    require_once '../Controller/ControllerUser.php';
+require_once '../Controller/ControllerProfileUBS.php';
+require_once '../Controller/ControllerUser.php';
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
@@ -13,11 +13,11 @@
         <script type="text/javascript" src="//j.maxmind.com/js/geoip.js"></script>
         <link href="../shared/css/jquery-ui-1.10.3.custom.css" rel="stylesheet">
         <link href="http://code.google.com/apis/maps/documentation/javascript/examples/default.css" rel="stylesheet" type="text/css" />
-        
+
         <?php
-        if(!isset($_REQUEST['lat'])){
-        ?>
-            <script type="text/javascript"> 
+        if (!isset($_REQUEST['lat'])) {
+            ?>
+            <script type="text/javascript">
                 navigator.geolocation.getCurrentPosition(showposUser);
                 function showposUser(position)
                 {
@@ -26,45 +26,44 @@
                     window.location = "ClosestUBS.php?lat=" + lat + "&lon=" + lon;
                 }
             </script>
-        <?php
-    }else{
-        ?>
-        <style>#mapview{display:none;}</style>
-        <title> Cadê Meu Hospital - Perfil UBS </title>
-    </head>
+            <?php
+        } else {
+            ?>
+            <style>#mapview{display:none;}</style>
+            <title> Cadê Meu Hospital - Perfil UBS </title>
+        </head>
 
-    <body>
-        
-        <div class="root">  
+        <body>
 
-            <?php 
+            <div class="root">  
+
+                <?php
                 require '../view/shared/header.php';
                 require '../view/shared/navigation_bar.php';
-                
+
                 $controllerUser = ControllerUser::getInstanceControllerUser();
                 $controllerProfileUBS = ControllerProfileUBS::getInstanceControllerProfileUBS();
                 $userLat = $_REQUEST['lat'];
                 $userLon = $_REQUEST['lon'];
-                $userCity = $controllerUser->takeCity($userLat,$userLon );
+                $userCity = $controllerUser->takeCity($userLat, $userLon);
                 $closestUBSs = $controllerProfileUBS->searchUBS($userCity->getCity(), 2);
                 $menor = 20000;
                 for ($i = 0; $i < count($closestUBSs); $i++) {
                     $currentUBS = $closestUBSs[$i];
 
-                    $distance =  $controllerProfileUBS->getDistanceBetweenTwoLatLon($userLat, $userLon ,$currentUBS->getLatitudeUBS() , $currentUBS->getLongitudeUBS());  
-       
+                    $distance = $controllerProfileUBS->getDistanceBetweenTwoLatLon($userLat, $userLon, $currentUBS->getLatitudeUBS(), $currentUBS->getLongitudeUBS());
+
                     if ($distance < $menor) {
                         $menor = $distance;
                         $closestUBS = $currentUBS;
                     }
                 }
+                ?>   
 
-            ?>   
+                <script>
+                    window.location = "Profile.php?id=<?php echo $closestUBS->getIdUBS(); ?>";
+                </script>
+        </body>
+    </html>
 
-            <script>
-                window.location="Profile.php?id=<?php echo $closestUBS->getIdUBS();?>";
-            </script>
-    </body>
-</html>
-
-    <?php } ?>
+<?php } ?>

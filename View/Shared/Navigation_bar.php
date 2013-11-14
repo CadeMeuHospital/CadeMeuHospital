51bca230858fd7ee61bfc21f25bec305cf552e47
atillagallio <atilla.gallio@gmail.com>
2013-11-14 16:01:08 -0200

    Refatoração view Navigation_bar.php

--- View/Shared/Navigation_bar.php
+++ View/Shared/Navigation_bar.php
@@ -4,7 +4,7 @@
             <ul>
                 <li><a href="../index.php">Home</a></li>
 
-                <li><a href="../View/ClosestUBS.php">UBS mais próxima a mim!</a></li>
+                <li><a href="../View/ClosestUBS.php">UBS mais próxima</a></li>
 
                 <li><a href="http://dados.gov.br/dataset/unidades-basicas-de-saude-ubs" target =" _blank">Dados Abertos</a></li>
                 

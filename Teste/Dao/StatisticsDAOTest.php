0ad94f2231792b8166d8b66205f5af5d62c93963
levimoraes <levi.moraes@yahoo.com.br>
2013-11-15 13:05:34 -0200

    Revisao StateDao NoSingleton

--- Teste/Dao/StatisticsDAOTest.php
+++ Teste/Dao/StatisticsDAOTest.php
@@ -7,21 +7,11 @@ class StatisticsDAOTest extends PHPUnit_Framework_TestCase {
     protected $statisticsDao;
 
     protected function setUp() {
-        $this->statisticsDao = StatisticsDAO::getInstanceStatisticsDAO();
+        $this->statisticsDao = new StatisticsDAO();
     }
 
     protected function tearDown() {
         unset($this->statisticsDao);
-    }
-
-    public function testGetInstanceStatisticsDAONotNull() {
-        $resultNotNull = $this->statisticsDao->getInstanceStatisticsDAO();
-        $this->assertNotNull($resultNotNull);
-    }
-
-    public function testGetInstanceStatisticsDAOInstanceOf() {
-        $resultInstanceOf = $this->statisticsDao->getInstanceStatisticsDAO();
-        $this->assertInstanceOf('StatisticsDAO', $resultInstanceOf);
     }
 
     public function testGetValuesToChartAverageEvaluate() {

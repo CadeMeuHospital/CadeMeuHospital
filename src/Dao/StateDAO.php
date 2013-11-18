f84f5ccbcd69575bb6a93ace29513c08b0874a67
Beatriz <beatrizrezener@gmail.com>
2013-11-18 20:11:12 -0200

    Teste StateDAO.php

--- src/Dao/StateDAO.php
+++ src/Dao/StateDAO.php
@@ -13,21 +13,17 @@ class StateDAO {
         $sqlAvgCount = "SELECT SUM(ubs.average) AS average, COUNT(ubs.average) AS quantity FROM ubs
                         INNER JOIN state, municipios_ibge 
                         WHERE state.acronym = municipios_ibge.uf
-                        AND state.acronym = '" . $stateAcronym . "' 
+                        AND state.acronym = '" . $stateAcronym[0] . "' 
                         AND municipios_ibge.codigo = ubs.cod_munic AND ubs.average <> 0";
 
         $resultAvgCount = mysql_query($sqlAvgCount);
         $arrayAvgCount = mysql_fetch_row($resultAvgCount);
 
-//        if ($arrayAvgCount[0] == null){
-//            return false;
-//        }
-
         if ($arrayAvgCount[1] > 0) {
             $average = ($arrayAvgCount[0]) / ($arrayAvgCount[1]);
-            $sql = "UPDATE state SET average = '" . $average . "' WHERE acronym = '" . $stateAcronym . "'";
+            $sql = "UPDATE state SET average = '" . $average . "' WHERE acronym = '" . $stateAcronym[0] . "'";
         } else {
-            $sql = "UPDATE state SET average = '" . $evaluate . "' WHERE acronym = '" . $stateAcronym . "'";
+            $sql = "UPDATE state SET average = '" . $evaluate . "' WHERE acronym = '" . $stateAcronym[0] . "'";
         }
 
         $result = mysql_query($sql);
@@ -42,7 +38,6 @@ class StateDAO {
     }
 
     //Novos metodo de busca
-
     public function takeStateUBSOO($codMunic) {
         $ufState = $this->takeUfStateUBS($codMunic);
         

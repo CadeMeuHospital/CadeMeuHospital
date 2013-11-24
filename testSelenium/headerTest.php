55b5fc04b1e13c88a6eedb7565319e63d5c7c109
levimoraes <levi.moraes@yahoo.com.br>
2013-11-23 22:13:11 -0200

    Teste da View de header

---
+++ testSelenium/headerTest.php
@@ -0,0 +1,18 @@
+<?php
+class Example extends PHPUnit_Extensions_SeleniumTestCase
+{
+  protected function setUp()
+  {
+    $this->setBrowser("*chrome");
+    $this->setBrowserUrl("http://localhost/");
+  }
+
+  public function testMyTestCase()
+  {
+    $this->open("/CadeMeuHospital/src/view/home.php");
+    $this->click("name=Enviar");
+    $this->waitForPageToLoad("30000");
+    $this->assertEquals("Campo não pode ser nulo!", $this->getAlert());
+  }
+}
+?>

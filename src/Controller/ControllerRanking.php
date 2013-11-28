dbfb3b9a570d5c73b81fdfbacd83023005d107d8
MaxOliveira <maxwell_oliveira2@hotmail.com>
2013-11-28 14:47:45 -0200

    Comentando classe

--- src/Controller/ControllerRanking.php
+++ src/Controller/ControllerRanking.php
@@ -10,6 +10,7 @@ class ControllerRanking {
         
     }
 
+    //Singleton Pattern
     public static function getInstanceControllerRanking() {
         if (!isset(self::$instanceControllerRanking)) {
             self::$instanceControllerRanking = new ControllerRanking();
@@ -17,16 +18,19 @@ class ControllerRanking {
         return self::$instanceControllerRanking;
     }
 
+    //Making a Rank
     public function makeRank() {
         $rankingDAO = RankingDAO::getInstanceRankingDAO();
         return $rankingDAO->getRank();
     }
     
+    //Making a Rank By City
     public function makeRankByCity($city) {
         $rankingDAO = RankingDAO::getInstanceRankingDAO();
         return $rankingDAO->getRankByCity($city);
     }
 
+    //Making a Rank By Neighborhood
     public function makeRankByNeighborhood($neighborhood){
         $rankingDAO = RankingDAO::getInstanceRankingDAO();
         return $rankingDAO->getRankByNeighborhood($neighborhood);

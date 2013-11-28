279f03b589d551960cbc8fa516b9a60b3d55729a
tiagorc <tiagobrasil2005@yahoo.com.br>
2013-11-28 15:48:04 -0200

    Refatoração da View 'Home.php'

--- src/View/Home.php
+++ src/View/Home.php
@@ -5,7 +5,6 @@ if (!isset($_POST['submit'])) {
     $rankType = $_POST['rank'];
     $textField = $_POST['textField'];
 }
-
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 
@@ -20,17 +19,17 @@ if (!isset($_POST['submit'])) {
         <script src="Shared/js/jquery.chocoslider.js" type="text/javascript"></script>
         <link rel="stylesheet" type="text/css" media="screen" href="Shared/css/estilo.css"/>
         <script type="text/javascript">
-                $(window).load(function() {
-                    $('#banner').chocoslider({
-                        autoPause: true,
-                        controlNavigation: true,
-                        numberStrips: 10,
-                        speedStrip: 1000,
-                        sliderDelay: 4000,
-                        transparencytitle: 0.6
-                    });
+            $(window).load(function() {
+                $('#banner').chocoslider({
+                    autoPause: true,
+                    controlNavigation: true,
+                    numberStrips: 10,
+                    speedStrip: 1000,
+                    sliderDelay: 4000,
+                    transparencytitle: 0.6
                 });
-            </script>
+            });
+        </script>
         <title> CMH - Home</title>
     </head>
 
@@ -42,135 +41,123 @@ if (!isset($_POST['submit'])) {
             require '../Controller/ControllerRanking.php';
             ?> 
 
-           
-                        <div align="center">
-                    <div class="content"> 
-                        
 
-                        <script type="text/javascript">
-                            $(document).ready(function() {
-                                $("#content div:nth-child(1)").show();
-                                $(".abas li:first div").addClass("selected");
-                                $(".aba").click(function() {
-                                    $(".aba").removeClass("selected");
-                                    $(this).addClass("selected");
-                                    var indice = $(this).parent().index();
-                                    indice++;
-                                    $("#content div").hide();
-                                    $("#content div:nth-child(" + indice + ")").show();
-                                });
-                                $(".aba").hover(function() {
-                                    $(this).addClass("ativa")
-                                }, function() {
-                                    $(this).removeClass("ativa")
-                                });
+            <div align="center">
+                <div class="content"> 
+
+
+                    <script type="text/javascript">
+                        $(document).ready(function() {
+                            $("#content div:nth-child(1)").show();
+                            $(".abas li:first div").addClass("selected");
+                            $(".aba").click(function() {
+                                $(".aba").removeClass("selected");
+                                $(this).addClass("selected");
+                                var indice = $(this).parent().index();
+                                indice++;
+                                $("#content div").hide();
+                                $("#content div:nth-child(" + indice + ")").show();
                             });
-                            $(elemento).hover(
-                                    function() {/*função a ser executada ao pôr o cursor sobre o elemento*/
-                                    },
-                                    function() {/*função a ser executada ao tirar o cursor do elemento*/
-                                    }
-                            );
+                            $(".aba").hover(function() {
+                                $(this).addClass("ativa")
+                            }, function() {
+                                $(this).removeClass("ativa")
+                            });
+                        });
+                        $(elemento).hover(
+                                function() {/*função a ser executada ao pôr o cursor sobre o elemento*/
+                                },
+                                function() {/*função a ser executada ao tirar o cursor do elemento*/
+                                }
+                        );
 
-                        </script>
+                    </script>
 
-                        <?php
-                        $controllerRanking = ControllerRanking::getInstanceControllerRanking();
-                        $topFiveArray = array();
-                        switch ($rankType) {
-                            case "geral":
-                                $topFiveUBS = $controllerRanking->makeRank();
-                                $topFiveUBSG = $topFiveUBS;
-                                break;
-                            case "cidade":
-                                $topFiveUBS = $controllerRanking->makeRankByCity($textField);
-                                $topFiveUBSC = $topFiveUBS;
-                                break;
-                            case "bairro":
-                                $topFiveUBS = $controllerRanking->makeRankByNeighborhood($textField);
-                                $topFiveUBSB = $topFiveUBS;
-                                break;
+                    <?php
+                    $controllerRanking = ControllerRanking::getInstanceControllerRanking();
+                    $topFiveArray = array();
+                    switch ($rankType) {
+                        case "geral":
+                            $topFiveUBS = $controllerRanking->makeRank();
+                            $topFiveUBSG = $topFiveUBS;
+                            break;
+                        case "cidade":
+                            $topFiveUBS = $controllerRanking->makeRankByCity($textField);
+                            $topFiveUBSC = $topFiveUBS;
+                            break;
+                        case "bairro":
+                            $topFiveUBS = $controllerRanking->makeRankByNeighborhood($textField);
+                            $topFiveUBSB = $topFiveUBS;
+                            break;
+                    }
+                    $numberUBS = mysql_num_rows($topFiveUBS);
+                    if ($numberUBS > 0) {
+                        for ($i = 0; $i < $numberUBS; $i++) {
+                            $nameUBS = mysql_result($topFiveUBS, $i, "nom_estab");
+                            $idUBS = mysql_result($topFiveUBS, $i, "cod_unico");
+                            $average = mysql_result($topFiveUBS, $i, "average");
+
+                            $path = "../view/Profile.php?id=" . $idUBS . "";
+                            $completePath = "<a href=" . $path . "> " . $nameUBS . " </a> - " . $average . "<br>";
+                            array_push($topFiveArray, $completePath);
                         }
-                        $numberUBS = mysql_num_rows($topFiveUBS);
-                        if ($numberUBS > 0) {
-                            for ($i = 0; $i < $numberUBS; $i++) {
-                                $nameUBS = mysql_result($topFiveUBS, $i, "nom_estab");
-                                $idUBS = mysql_result($topFiveUBS, $i, "cod_unico");
-                                $average = mysql_result($topFiveUBS, $i, "average");
-
-                                $path = "../view/Profile.php?id=" . $idUBS . "";
-                                $completePath = "<a href=" . $path . "> " . $nameUBS . " </a> - " . $average . "<br>";
-                                array_push($topFiveArray, $completePath);
-                            }
-              
-                        } else {
-                            echo 'Não há UBSs avaliadas. ';
-                        }
-                        ?>
-						
-						 <table width="760">
-                <tr>
-                    <td>
-                        <div class="banner" id="banner" >
-                            <a href="http://www.fga.unb.br/"><img src="Shared/img/banner1.png" alt="UnB Gama" title="Visite a WebSite da FGA!"/></a>
-                            <a href="#"><img src="Shared/img/banner2.jpg" alt="Imagem 2" title="Texto da imagem 2"/></a>
-                            <a href="#"><img src="Shared/img/banner4.jpg" alt="Imagem 4" title="Texto da imagem 4"/></a>
-                            <a href="http://cnes.datasus.gov.br/" target =" _blank">
-                                <img src="Shared/img/banner2.png"alt="Imagem 4" title="CNES"/>
-                            </a>
-                            <a href="http://www.saudeparatodosdf.com.br/" target =" _blank2">
-                                <img src="Shared/img/banner4.png"alt="Imagem 4" title="Saúde para Todos!"/>
-                            </a>
-                            <a href="http://www2.datasus.gov.br/DATASUS/index.php" target =" _blank1">
-                                <img src="Shared/img/banner3.png" alt="Imagem 4" title="Data SUS"/></a>
-                        </div>
-                    </td>
-                    <td>
-						
-                        <right>
-                            <div class="Outline1">
-                                <div id="text">Classificacao &nbsp <img 
-                                        src="../view/shared/img/cmhRankTitle.png" align="right" /></div>
-                                <div class="Outline2">
-                                    <div class="TabControl">
-                                        <div id="header">
-                                            <ul class="abas">
-                                                <li>
-                                                    <div class="aba">
-                                                        <span style="position: relative; top: 10px;
-                                                              left: 100 px;"> Geral</span>
-                                                    </div>
-                                                </li>
-                                            </ul>
-                                        </div>
-                                        <div id="content">
-                                            <div class="conteudo">
-                                                <?php
-                                                $countArray = count($topFiveArray);
-                                                for ($i = 0; $i < $countArray; $i++) {
-                                                    echo '<div class="ubs">';
-                                                    echo '<div id="h1">';
-                                                    echo $topFiveArray[$i] . "<br>";
-                                                    echo '</div>';
-                                                    echo '</div>';
-                                                }
-                                                ?>
+                    } else {
+                        echo 'Não há UBSs avaliadas. ';
+                    }
+                    ?>
+                    <div class="banner" id="banner" >
+                        <a href="http://www.fga.unb.br/"><img src="Shared/img/banner1.png" alt="UnB Gama" title="Visite a WebSite da FGA!"/></a>
+                        <a href="#"><img src="Shared/img/banner2.jpg" alt="Imagem 2" title="Texto da imagem 2"/></a>
+                        <a href="#"><img src="Shared/img/banner4.jpg" alt="Imagem 4" title="Texto da imagem 4"/></a>
+                        <a href="http://cnes.datasus.gov.br/" target =" _blank">
+                            <img src="Shared/img/banner2.png"alt="Imagem 4" title="CNES"/>
+                        </a>
+                        <a href="http://www.saudeparatodosdf.com.br/" target =" _blank2">
+                            <img src="Shared/img/banner4.png"alt="Imagem 4" title="Saúde para Todos!"/>
+                        </a>
+                        <a href="http://www2.datasus.gov.br/DATASUS/index.php" target =" _blank1">
+                            <img src="Shared/img/banner3.png" alt="Imagem 4" title="Data SUS"/></a>
+                    </div>
+                    <div class="Outline1">
+                        <div id="text">Classificacao &nbsp <img 
+                                src="../view/shared/img/cmhRankTitle.png" align="right" /></div>
+                        <div class="Outline2">
+                            <div class="TabControl">
+                                <div id="header">
+                                    <ul class="abas">
+                                        <li>
+                                            <div class="aba">
+                                                <span style="position: relative; top: 10px;
+                                                      left: 100 px;"> Geral</span>
                                             </div>
-                                        </div>
+                                        </li>
+                                    </ul>
+                                </div>
+                                <div id="content">
+                                    <div class="conteudo">
+                                        <?php
+                                        $countArray = count($topFiveArray);
+                                        for ($i = 0; $i < $countArray; $i++) {
+                                            echo '<div class="ubs">';
+                                            echo '<div id="h1">';
+                                            echo $topFiveArray[$i] . "<br>";
+                                            echo '</div>';
+                                            echo '</div>';
+                                        }
+                                        ?>
                                     </div>
                                 </div>
                             </div>
+                        </div>
+                    </div>
 
-                        </right>
-
-                    </div>        
                 </div>
-                    </td>
-                </tr>
-            </table>
-            <br>
-            <br>
-            <?php require '../view/shared/footer.php'; ?>
+                <br>
+                <br>
+                <br>
+                <br>
+                <?php require '../view/shared/footer.php'; ?>
+            </div>
         </div>
     </body>
 </html>

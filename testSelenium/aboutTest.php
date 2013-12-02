99df605d8c1d42a5d8c6757526a7e4d1aac5782a
tiagorc <tiagobrasil2005@yahoo.com.br>
2013-12-01 23:24:35 -0300

    Teste da About.php

---
+++ testSelenium/aboutTest.php
@@ -0,0 +1,17 @@
+<?php
+class Example extends PHPUnit_Extensions_SeleniumTestCase
+{
+  protected function setUp()
+  {
+    $this->setBrowser("*chrome");
+    $this->setBrowserUrl("http://localhost/");
+  }
+
+  public function testMyTestCase()
+  {
+    $this->open("/cademeuhospital/src/View/Home.php");
+    $this->click("link=Sobre");
+    $this->assertTitle("CMH - Sobre");
+  }
+}
+?>

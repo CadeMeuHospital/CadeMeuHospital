9478a3d28b3cd9513cf7c552afacc75aacb0a9e3
tiagorc <tiagobrasil2005@yahoo.com.br>
2013-12-01 23:48:09 -0300

    Evolucao do Teste da About.php

--- testSelenium/aboutTest.php
+++ testSelenium/aboutTest.php
@@ -1,16 +1,20 @@
 <?php
-class Example extends PHPUnit_Extensions_SeleniumTestCase
+class About extends PHPUnit_Extensions_SeleniumTestCase
 {
   protected function setUp()
   {
     $this->setBrowser("*chrome");
     $this->setBrowserUrl("http://localhost/");
+    
   }
 
-  public function testMyTestCase()
+  public function testAboutPage()
   {
     $this->open("/cademeuhospital/src/View/Home.php");
+    $this->click("css=img");
+    $this->waitForPageToLoad("30000");
     $this->click("link=Sobre");
+    $this->waitForPageToLoad("30000");
     $this->assertTitle("CMH - Sobre");
   }
 }

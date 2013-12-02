d19d8cc72ea911e4ccf27432e72bdd41200ac6e2
paulohtfs <paulohtfs@gmail.com>
2013-12-01 22:58:34 -0200

    Adicao de informacoes na ´Sobre´

---
+++ src/View/About.php
@@ -0,0 +1,68 @@
+﻿<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<link rel="shortcut icon" href="Shared/img/favicon.ico">
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <link rel="stylesheet" href="../View/Shared/css/style.css" type="text/css">
+        <link rel="stylesheet" href="css/home.css" type="text/css">
+        <script type="text/javascript" src="/../V.iew/Shared/js/jquery.price_format.1.8.min.js"></script>
+        <link href="/../Shared/css/jquery-ui-1.10.3.custom.css" rel="stylesheet">
+        <title> CMH - Sobre</title>
+    </head>
+
+    <body align="center">
+        <div class="root">                   
+            <?php 
+                require '../View/Shared/Header.php';
+                require '../View/Shared/Navigation_bar.php'; 
+            ?>
+            <div align ="center">
+                <div class="content">
+                    <table border = "1">                    
+                        <b><font size="6">Lei de Acesso à Informação (LAI)</font></b>
+                        <table width="760"border="0">
+                           
+                            <font size="05"><p align="justify">    
+                                    Segundo o <a href="http://www.acessoainformacao.gov.br/"  target="_blank"> site oficial</a>, a LAI regulamenta o direito, previsto
+                                na Constituição, de qualquer pessoa solicitar e receber dos órgãos e
+                                entidades públicos, de todos os entes e Poderes, informações públicas
+                                por eles produzidas ou custodiadas. Através da mesma, é possível se obter 
+                               dados (processados ou não), que podem ser utilizados para produção e transmissão
+                               de conhecimento, registrados em qualquer suporte ou formato.
+                               </p></font>
+                        </table>
+                        <br /><br /><br />
+
+                        <b><font size="6">Unidade Básica de Saúde (UBS)</font></b>
+                        <table width="760"border="0">
+                           <font size="05"><p align="justify">
+                                   O propósito da criação das UBS é atender aproximadamente 80% dos pacientes
+                                com problemas de saúde, que não são tão graves e não necessitam ser 
+                               encaminhados para o hospital, sendo assim, o fluxo de pacientes, que apresentam
+                                um quadro mais grave, nos hospitais deverá reduzir consideravelmente, elas 
+                               fazem parte da Política Nacional de Urgência e Emergência, lançado em 2013 
+                               pelo Governo Federal. Preferencialmente, as UBS são construídas nas cidades 
+                               mais pobres, terão prioridade os municípios que participam do Programa Brasil 
+                               Sem Miséria. Nas UBS os usuários do SUS podem realizar consultas médicas básicas,
+                                como por exemplo, Pediatria, Ginecologia com exames de gravidez muito mais rápido, 
+                               aproximadamente em 5 minutos já sai o resultado, diferentemente dos exames convencionais
+                                que levam dias para sair o resultado, Odontologia, Clinica Geral com curativos simples, 
+                               tomar vacinas e coletar exames laboratoriais
+                               </p></font>     
+
+                        </table>
+
+                </div>
+
+
+                <br /><br />
+                    <a href="http://dados.gov.br/dataset/unidades-basicas-de-saude-ubs" target="_blank"><p align="right">Saiba Mais</p></a>
+                
+                <br /><br /><br /><br /><br />
+                <?php require '../View/Shared/Footer.php'; ?>
+            </div>
+
+
+        </div>
+    </body>
+</html>
